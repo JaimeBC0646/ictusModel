e2d5a16ec6d95622f4c16d02d395ec711e12d956
Jimmy <147665532+JaimeBC0646@users.noreply.github.com>
2024-06-06 03:28:26 -0600

    Last commit 06-06-24

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Test using venv on VSCode and pushing with git"
+    return render_template('datos_Form.html')
+
+
+@app.route('/img/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('img', filename)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f"<h1>Buen dia: {name} :3.</h1> \n <a href='/'>Volver</a>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
